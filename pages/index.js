5c440cfd38e402ae6ea225f4b1353182c48de096
s22002 <s22002@std.it-college.ac.jp>
2023-02-14 08:10:08 +0000

    chapter2 finished

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
+import Hero from 'components/hero'
+
 const Home = () => {
   return (
-    <>
-      <header>Header</header>
-
-      <main>
-        <div>
-          <h1>CUBE</h1>
-          <p>アップしていくサイト</p>
-        </div>
-      </main>
-
-      <footer>Footer</footer>
-
-    </>
+    <Hero
+      title='CUBE'
+      subtitle='アップしていくサイト'
+      imageOn
+    />
   )
 }
 
