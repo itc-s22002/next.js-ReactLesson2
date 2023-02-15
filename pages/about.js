b135218e18e230f2104e02f0570f742370d22807
s22002 <s22002@std.it-college.ac.jp>
2023-02-15 05:12:13 +0000

    3.6 global style

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Hero from 'components/hero'
+
+const Home = () => {
+  return (
+    <Hero
+      title='About'
+      subtitle='About development activities'
+    />
+  )
+}
+
+export default Home
---
+++ pages/blog/index.js
@@ -0,0 +1,12 @@
+import Hero from 'components/hero'
+
+const Home = () => {
+  return (
+    <Hero
+      title='Blog'
+      subtitle='React Posts'
+    />
+  )
+}
+
+export default Home
