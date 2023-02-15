970f5595ef4fe0b3daba4e4a447d8d40feade6a2
s22002 <s22002@std.it-college.ac.jp>
2023-02-15 05:41:24 +0000

    4.2 header layout

---
+++ components/header.js
@@ -0,0 +1,14 @@
+import Logo from 'components/logo'
+import Nav from 'components/nav'
+import styles from 'styles/header.module.css'
+const Header = () => {
+  return (
+    <header>
+      <div className={styles.flexContainer}>
+        <Logo boxOn />
+        <Nav />
+      </div>
+    </header>
+  )
+}
+export default Header
