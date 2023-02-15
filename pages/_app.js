2286e30f14f1550b8a6468075035b062c268f5e1
s22002 <s22002@std.it-college.ac.jp>
2023-02-15 08:09:49 +0000

    5.9ソーシャルリンクメニュー

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import 'styles/globals.css'
 import Layout from 'components/layout'
 
+// Font Awesomeの設定
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 const App = ({ Component, pageProps }) => {
   return (
     <Layout>
