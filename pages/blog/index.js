c47836a99659f4a875b7b2ef695183028575f918
s22002 <s22002@std.it-college.ac.jp>
2023-02-16 05:28:37 +0000

    6.4

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,11 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 
 const Home = () => {
   return (
     <Container>
+      <Meta pageTitle="ブログ" />
       <Hero
         title='Blog'
         subtitle='React Posts'
