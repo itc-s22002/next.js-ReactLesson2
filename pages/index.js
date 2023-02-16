f5d122e4348507804d3c6ffaa2d859ce8b266ea3
s22002 <s22002@std.it-college.ac.jp>
2023-02-16 16:17:18 +0000

    9.8

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
 
-const Home = () => {
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+const Home = ({ posts }) => {
   return (
     <Container>
       <Meta />
@@ -11,8 +18,27 @@ const Home = () => {
         subtitle='アップしていくサイト'
         imageOn
       />
+
+      <Posts posts={posts} />
+      <Pagination nextUrl='/blog' nextText='More Posts' />
     </Container>
   )
 }
 
+export async function getStaticProps () {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+  return {
+    props: {
+      posts
+    }
+  }
+}
 export default Home
