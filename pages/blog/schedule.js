2d0dbbf2c23b34e88534267c2716641ee42e26e7
s22002 <s22002@std.it-college.ac.jp>
2023-02-16 08:03:50 +0000

    8.6

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,14 +1,24 @@
 import { getPostBySlug } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
 import PostBody from 'components/post-body'
 import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
 import ConvertBody from 'components/convert-body'
+import PostCategories from 'components/post-categories'
 import Image from 'next/image'
 
-const Schedule = ({ title, publish, content, eyecatch, categories }) => {
+const Schedule = ({ title, publish, content, eyecatch, categories, description }) => {
   return (
     <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
       <article>
         <PostHeader title={title} subtitle='Blog Article' publish={publish} />
         <figure>
@@ -31,7 +41,9 @@ const Schedule = ({ title, publish, content, eyecatch, categories }) => {
               <ConvertBody contentHTML={content} />
             </PostBody>
           </TwoColumnMain>
-          <TwoColumnSidebar />
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
         </TwoColumn>
       </article>
     </Container>
@@ -43,13 +55,15 @@ export default Schedule
 export async function getStaticProps () {
   const slug = 'schedule'
   const post = await getPostBySlug(slug)
+  const description = extractText(post.content)
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
       eyecatch: post.eyecatch,
-      categories: post.categories
+      categories: post.categories,
+      description
     }
   }
 }
