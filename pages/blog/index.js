52ea746fcc548e2d3a80d917e065f0164068023f
s22002 <s22002@std.it-college.ac.jp>
2023-02-16 15:38:55 +0000

    9.5

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,10 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
 
-const Home = () => {
+const Blog = ({ posts }) => {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
@@ -10,8 +12,19 @@ const Home = () => {
         title='Blog'
         subtitle='React Posts'
       />
+      <Posts posts={posts} />
     </Container>
   )
 }
 
-export default Home
+export async function getStaticProps () {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Blog
