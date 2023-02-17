ff081dd533103cc504d2627d55cf0f6f18206b89
s22002 <s22002@std.it-college.ac.jp>
2023-02-17 14:52:45 +0000

    10.3

--- components/convert-date.js
+++ components/convert-date.js
@@ -1,14 +1,15 @@
 import { parseISO, format } from 'date-fns'
 import ja from 'date-fns/locale/ja'
 
-const ConvertDate = ({ dateISO }) => {
+const ConverDate = ({ dateISO }) => {
   return (
-    <time dataTime={dateISO}>
+    <time dateTime={dateISO}>
       {format(parseISO(dateISO), 'yyyy年MM月dd日', {
         locale: ja
       })}
     </time>
+
   )
 }
 
-export default ConvertDate
+export default ConverDate
